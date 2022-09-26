63d813910db5b72918af43cb51dd30aec6f19b25
prabhat_021 <prabhatsahrawat010203@gmail.com>
2022-09-27 00:46:35 +0530

    updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express =require('express');
 const cookieParser = require('cookie-parser')
 const app = express();
-const port = 8000;
+const port = process.env.port || 8000;
 
 var flash = require('connect-flash');
 
